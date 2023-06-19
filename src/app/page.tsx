a83cd0627cf5902e7336de0805f1b2ee989da8fe
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2023-06-19 15:48:48 +0100

    adding work on the applications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,80 @@
-// @ts-nocheck
+// @ts-ignore
 "use client";
 
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const ADDI_AUTHENTICATION_TEST_URL = "https://api.staging.addi.com";
 
 const ADDI_API_TEST_URL = "https://api.addi-staging.com";
 
+type AddiToken = {
+  access_token: string;
+  scope: string;
+  expires_in: number;
+  token_type: string;
+};
+
 export default function Home() {
-  useEffect(() => {
-    const getAddiAuthorization = async () => {
-      const body = {
-        grant_type: "client_credentials",
-        client_id: process.env.NEXT_PUBLIC_CLIENT_ID || "",
-        client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET || "",
-      };
-      fetch(`${ADDI_AUTHENTICATION_TEST_URL}/oauth/token`, {
-        body: JSON.stringify(body),
-        method: "POST",
-      })
-        .then((response) => {
-          let info = response.json();
-          // debugger;
-        })
-        .catch((err) => {
-          // debugger;
-        });
-    };
+  const [jwt, setJwt] = useState<AddiToken>({
+    access_token: "",
+    scope: "",
+    expires_in: 0,
+    token_type: "",
+  });
+  const [price, setPrice] = useState<number>(0);
 
-    getAddiAuthorization();
-  }, []);
-
-  const createApplication = useCallback(() => {
-    // change todo
+  const getAddiAuthorization = async () => {
     const body = {
       grant_type: "client_credentials",
       client_id: process.env.NEXT_PUBLIC_CLIENT_ID || "",
       client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET || "",
     };
+
     fetch(`${ADDI_AUTHENTICATION_TEST_URL}/oauth/token`, {
       body: JSON.stringify(body),
       method: "POST",
-      // headers: {
-      //   Authorization: `Beater ${JWT}`,
-      // },
+    })
+      .then(async (response) => {
+        let info = await response.json();
+        debugger;
+        setJwt(info);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const createApplication = useCallback(() => {
+    // change todo
+    const body = {
+      orderId: "",
+      totalAmount: price,
+      shippingAmount: 0,
+      currency: "COP", // only supported COP
+      items: [],
+      suppliers: [],
+      client: {},
+      allyUrlRedirection: {
+        logoUrl: "",
+        useCallbackUrl: "",
+        redirectionUrl: "",
+      },
+      // optional
+      // totalTaxesAmount: 0,
+      // shippingAddress: {},
+      // billingAddress: {},
+      // pickUpAddress: {},
+      // geoLocation: {}
+    };
+
+    if (!jwt || !jwt.access_token) return;
+
+    fetch(`${ADDI_AUTHENTICATION_TEST_URL}/v2/online-applications`, {
+      body: JSON.stringify(body),
+      method: "POST",
+      headers: {
+        Authorization: `Bearer: ${jwt}`,
+      },
     })
       .then((response) => {
         let info = response.json();
@@ -52,17 +83,39 @@ export default function Home() {
       .catch((err) => {
         // debugger;
       });
-  }, []);
+  }, [jwt]);
 
-  useEffect(() => {
-    createApplication();
-  }, [createApplication]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white dark:bg-white">
-      <div>here</div>
+    <main className="flex min-h-screen flex-col items-center gap-5 p-24 dark:text-white text-black">
+      <div className="flex gap-2">
+        <label>Price</label>
+        <input
+          value={price}
+          onChange={(e) => setPrice(Number(e.target.value))}
+        ></input>
+      </div>
+      <div className="flex flex-col gap-1">
+        <div>JWT</div>
+        <p>{jwt.access_token || ""}</p>
+      </div>
+      <div className="flex gap-5">
+        <button
+          className="border border-purple-400 p-2"
+          onClick={() => getAddiAuthorization()}
+        >
+          Get Authorization
+        </button>
+        <button
+          className="border border-purple-400 p-2"
+          onClick={() => createApplication()}
+        >
+          Get Authorization
+        </button>
+      </div>
       <div>
+        {/* tslint:disable-next-line */}
         <addi-widget
-          price="239900"
+          price={price}
           ally-slug="tennis-ecommerce"
           custom-widget-styles='{"widget":{"borderColor":"#000","borderRadius":"0px","fontColor":"#000","fontFamily":"Lato-Regular","fontSize":"14px","badgeBackgroundColor":"#fff","infoBackgroundColor":"#fff","margin":"15px%200"},"modal":{"backgroundColor":"#fff","fontColor":"#000","fontFamily":"Lato-Regular","priceColor":"#000","badgeBackgroundColor":"#fff","badgeFontColor":"#000","badgeBorderRadius":"0px","cardColor":"#fff","buttonBorderColor":"#000","buttonBorderRadius":"0px","buttonBackgroundColor":"#000","buttonFontColor":"#fff"}}'
         >
