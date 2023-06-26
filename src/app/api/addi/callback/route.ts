e708ae3b72dd73e99154652793b6ad38b358b543
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2023-06-26 18:38:09 +0100

    adding console log

--- src/app/api/addi/callback/route.ts
+++ src/app/api/addi/callback/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const body = await request.json();
+  console.log(body);
   await supabaseAdmin
     .from("callbacks")
     .insert({ id: uuidv4(), metadata: { message: "received" } });
