befbe70a54cf4b151b007bfda5fc34e1b653651d
Paulo Gonçalves Cardoso <pcardoso.lei@gmail.com>
2023-06-26 18:18:39 +0100

    changing the checkout

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,5 +1,9 @@
 const CheckoutPage = () => {
-  return <div>MOCK CHECKOUT HERE</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-5 p-24 dark:text-white text-black">
+      Redirect Checkout Page here.
+    </main>
+  );
 };
 
 export default CheckoutPage;
